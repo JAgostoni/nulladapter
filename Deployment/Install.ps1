72faa140de3cdce8759fa9f94e51ca7efc6febbf
Jason Agostoni <jason@agostoni.net>
2016-02-10 09:20:45 -0500

    Created initial installer script

---
+++ Deployment/Install.ps1
@@ -0,0 +1,34 @@
+﻿$SourceDLL = "..\NullAdapter\bin\Release\Winterdom.BizTalk.Adapters.NullAdapter.dll";
+
+
+#Test for 64 bit
+$Is64Bit =  [Environment]::Is64BitProcess;
+
+#Install Path
+$InstallPath = "C:\Program Files\BizTalkNullAdapter\";
+If($Is64Bit) 
+{
+    Write-Host "64-bit Process detected."
+    $InstallPath = "C:\Program Files (x86)\BizTalkNullAdapter\"; 
+}
+
+#Copy RELEASE version
+If(Test-Path $SourceDLL) 
+{
+    Write-Host "Release version of adapter DLL found"
+    xcopy /Y $SourceDLL $InstallPath
+    
+    #Registry entries
+    Write-Host "Merging registry entries";
+    regedit /S .\NullAdapter.reg
+
+    if($Is64Bit) {
+        Write-Host "Merging 32-bit on 64-bit registry entries";
+        regedit /S .\NullAdapter64.reg
+    }
+
+    #Install UTIL to register the event sources
+
+} else {
+    Write-Error "Release version of adapter DLL NOT found. Please build the VS project first in RELEASE mode."
+}
